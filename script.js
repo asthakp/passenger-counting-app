b1d203302e622d38c7f83613e282c4034c928285
Astha <aasudagreat@gmail.com>
2023-01-30 20:15:21 +0545

    complete java

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let numOfPassengers= document.getElementById("number")
-let buttonUp=document.getElementById("increased")
-let count=0
-buttonUp.addEventListener("click", ()=>{
-    count+=1
-    numOfPassengers.textContent=count    // textContent has an advantage over innerText that                                           //it can read hidden strings and msgs like space.
+let numOfPassengers = document.getElementById("number")
+let buttonUp = document.getElementById("increased")
+let count = 0
+buttonUp.addEventListener("click", () => {
+    count += 1
+    numOfPassengers.textContent = count
+})
+buttonSave.addEventListener("click", () => {
+    saveEl.textContent += count + " - "
+    numOfPassengers.textContent = 0
+    count = 0
 })
